a32cace019e1db4779fccd7116665da900e8bd29
Jugge <Jugge@LAPTOP-QE8J3JIU>
2024-01-08 22:28:36 -0600

    Agregar archivos de proyecto.

---
+++ Prueba/Form1.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Windows.Forms;
+
+namespace Prueba
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Label myLabel = new Label();
+            myLabel.Text = "Diego Diaz\n 166129\n Graficacion y videojuegos";
+            myLabel.Dock = DockStyle.Fill;
+            myLabel.TextAlign = ContentAlignment.MiddleCenter;
+
+            Controls.Add(myLabel);
+        }
+    }
+}
+
